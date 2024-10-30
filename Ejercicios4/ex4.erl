396c3a462e4c85711151975db964cf62c4425df9
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-10-30 22:07:54 +0100

    ex$-binary tree fold

--- Ejercicios4/ex4.erl
+++ Ejercicios4/ex4.erl
@@ -1,5 +1,6 @@
 -module(ex4).
--export([list_even/1,list_square/1,list_even_square/1]).
+-export([list_even/1,list_square/1,
+    list_even_square/1,fold_tree/3, sum_tree/1]).
 
 list_even(N) ->
     [T || T <- N, T rem 2 == 0].
@@ -9,3 +10,34 @@ list_square(N) ->
 
 list_even_square(N) ->
     [T * T || T <- N, T rem 2 == 0].
+
+
+
+
+
+fold_tree(F,I,L) ->
+    F(I,fold_tree(F,L)).
+fold_tree(_,[V|[[]|[[]]]]) ->
+    io:format("d:~w  \n",[V]),
+    V;
+fold_tree(F,[V|[[]|[H3|_]]]) ->
+    B = fold_tree(F,H3),
+    io:format("c:~w ~w \n",[V,B]),
+    F(V,B);
+fold_tree(F,[V|[H2|[[]|_]]]) ->
+    A = fold_tree(F,H2),
+    io:format("b:~w ~w \n",[V,A]),
+    F(V,A);
+fold_tree(F,[V|[H2|[H3|_]]]) ->
+    A = fold_tree(F,H2),
+    B = fold_tree(F,H3),
+    io:format("a1:~w ~w ~w ~w \n",[V,A, B,F(A,B)]),  
+    F(V,F(A,B)).
+
+
+
+sum_tree(L) ->
+    fold_tree(
+        fun(Ac,V) -> Ac / V  end,
+        1,L).
+
