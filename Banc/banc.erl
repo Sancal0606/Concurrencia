b2497d2499bd6c42bc31f1b9b21a3ec16488213a
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-12-17 19:46:08 +0100

    ultra bank

--- Banc/banc.erl
+++ Banc/banc.erl
@@ -1,5 +1,5 @@
 -module(banc).
--export([start/0,do_op/1,consulta/1,ingreso/2,reintegra/2]).
+-export([start/0,do_op/1,consulta/1,ingreso/2,reintegra/2,avorta/1,esperar/0]).
 
 start() ->
     spawn(?MODULE,do_op,[1]).
@@ -7,24 +7,29 @@ start() ->
 do_op(D) ->
     receive
         {From,consulta} -> 
-            timer:sleep(700),
+            timer:sleep(esperar()),
             From ! D,
+            term(),
             do_op(D);
         {From, ingreso, Q} ->
-            timer:sleep(700),
+            timer:sleep(esperar()),
             D2 = D + Q,
             From ! D2,
             do_op(D2);
         {From, reintegra, Q} when Q =< D ->
-            timer:sleep(700),
+            timer:sleep(esperar()),
+            esperar(),
             D2 = D - Q,
             From ! D2,
             do_op(D2);
         {From, reintegra, _} ->
-            timer:sleep(700),
+            timer:sleep(esperar()),
             From ! denegada,
             do_op(D)
+
     end.
+
+esperar() -> 1000 * trunc(2 * math:pow(-math:log(1 - rand:uniform()),0.5)).
 
 consulta(B) ->
     B ! {self(),consulta},
@@ -41,5 +46,11 @@ ingreso(B,Q) ->
 reintegra(B,Q) ->
     B ! {self(),reintegra,Q},
     receive
+        X -> X
+    end.
+
+avorta(B)->
+    B ! {self(),avorta},
+    receive 
         X -> X
     end.
--- Banc/banc/banc1.erl
+++ Banc/banc/banc1.erl
@@ -5,49 +5,19 @@ start(Idb) -> spawn(?MODULE, do_op, [0,Idb]).
 
 do_op(D,Idb) ->
     receive
-	{From, consulta} ->
-	    Delay = add_delay(),
-	    if Delay > 5 ->
-		    timer:sleep(timer:seconds(Delay))
-		    From ! {Idb, D},
-		    do_op(D,Idb);
-	       true ->
-		     do_op(D,Idb)
-	    end,
-	    
-	{From, ingressa, Q} ->
-	    Delay = add_delay(),
-	    if Delay >= 5 ->
-		    term();
-	       true ->
-		    timer:sleep(timer:seconds(Delay)) 
-	    end,
-	    D2 = D+Q,
-	    From ! {Idb, D2},
-	    do_op(D2,Idb);
-	{From, reintegra, Q} when Q =< D ->
-	    Delay = add_delay(),
-	    if Delay >= 5 ->
-		    term();
-	       true ->
-		    timer:sleep(timer:seconds(Delay)) 
-	    end,
-	    D2 = D-Q,
-	    From ! {Idb, D2},
-	    do_op(D2,Idb);
-	{From, reintegra, _} ->
-	    Delay = add_delay(),
-	    if Delay >= 5 ->
-		    term();
-	       true ->
-		    timer:sleep(timer:seconds(Delay)) 
-	    end,
-	    From ! {Idb, denegada},
-	    do_op(D,Idb)
+		{From, consulta} ->
+			Delay = add_delay(),
+			if Delay > 5 ->
+				term();
+			true ->
+				do_op(D,Idb)
+			
+		
     end.
 
 add_delay() ->
     trunc(2*(math:pow((-math:log(rand:uniform())),0.5))).
+esperar() -> trunc(2 * math:pow(-math:log(1 - rand:uniform()),0.5)).
 
 consulta(B) ->
     B ! {self(), consulta},
